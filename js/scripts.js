994fb1eccea305a50b916c009addfeca343fd4d0
jb <jessb.epicodus@gmail.com>
2022-01-18 13:57:27 -0800

    refactor using loops;
    Co-Authored by: Marisa Edgar <marisaedgar1212@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// $(document).ready(function() {
+//   console.log("Script executing!")
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+//     console.log("Submit form successfully reached.");
+//     const person1Input = $("input#person1").val();
+//     const person2Input = $("input#person2").val();
+//     const animalInput = $("input#animal").val();
+//     const exclamationInput = $("input#exclamation").val();
+//     const verbInput = $("input#verb").val();
+//     const nounInput = $("input#noun").val();
+
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".exclamation").text(exclamationInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+
+//     $("#story").show();
+//   });
+// });
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
@@ -8,13 +31,15 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
+    let inputsArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
+    
+    let outputsArray = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"];
+    
+    let i =0;
+    inputsArray.forEach(function(element){
+      $(outputsArray[i]).text(element);
+      i ++;
+    });
     $("#story").show();
   });
 });
